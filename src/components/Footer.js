6355f63c15db7386bdee2b2c13cc5d9bafd2f97e
Giclayne <giclayne@gmail.com>
2022-12-13 20:10:18 -0300

    importando o css com classname em footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import {NavLink} from "react-router-dom"
 import styles from "./Footer.module.css";
 const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <h3>Escreva sobre o que tem interesse</h3>
       <p>Miniblog &copy; 2022</p>
     </footer>
