8d3ec39d12188b74baf7f45e6013b0d95b4e8471
Giclayne <giclayne@gmail.com>
2022-12-14 15:49:31 -0300

    importacao das rotas de dashboard e createPost

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import About from "./pages/About/About";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import { AuthProvider } from "./context/AuthContext";
+import CreatePost from "./pages/CreatePost/CreatePost";
+import Dashboard from "./pages/Dashboard/Dashboard"
 function App() {
 
   const [user,setUser] = useState(undefined)
@@ -38,6 +40,8 @@ function App() {
               <Route path="/about" element={<About />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
+              <Route path="/posts/create" element={<CreatePost />} />
+              <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
           </div>
           <Footer />
