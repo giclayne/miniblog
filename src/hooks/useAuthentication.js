070c25d70ea355570efba42c03ed7a1db1427d26
Giclayne <giclayne@gmail.com>
2022-12-14 00:35:58 -0300

    registrando usuario no sistema

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -23,4 +23,35 @@ export const useAuthentication = () => {
       return;
     }
   }
+
+  const createUser = async (data) => {
+    checkIfIsCancelled();
+
+    setLoading(true);
+    try {
+      const { user } = createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+      await updateProfile(user, {
+        displayName: data.displayName,
+      });
+      return user;
+    } catch (error) {
+      console.log(error.message);
+      console.log(typeof error.message);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    return () => setCancelled(true);
+  }, []);
+  return {
+    auth,
+    createUser,
+    error,
+    loading,
+  };
 };
