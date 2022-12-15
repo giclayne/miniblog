b611ad3d92ec9c1691a0fa0681e18f9f809874bb
Giclayne <giclayne@gmail.com>
2022-12-15 16:53:40 -0300

    pagina de exibicao da query/tag

---
+++ src/pages/Search/Search.js
@@ -0,0 +1,16 @@
+import React from 'react'
+//hooks
+import { useFetchDocuments } from '../../hooks/useFetchDocument'
+import { useQuery } from '../../hooks/useQuery'
+const Search = () => {
+    const query = useQuery()
+    const search = query.get("q")
+  return (
+    <div>
+        <h2>Search</h2>
+        <p>{search}</p>
+    </div>
+  )
+}
+
+export default Search
