f2044bf299c146de474f696163ccc4942ddfd679
Giclayne <giclayne@gmail.com>
2022-12-15 16:54:20 -0300

    importacao da pagina de busca

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Register from "./pages/Register/Register";
 import { AuthProvider } from "./context/AuthContext";
 import CreatePost from "./pages/CreatePost/CreatePost";
 import Dashboard from "./pages/Dashboard/Dashboard";
+import Search from "./pages/Search/Search";
 function App() {
   const [user, setUser] = useState(undefined);
   const { auth } = useAuthentication();
@@ -37,6 +38,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
+              <Route path="/search" element={<Search />} />
               <Route
                 path="/login"
                 element={!user ? <Login /> : <Navigate to="/" />}
