2e4bdaec28e7fad09f07a986976f3bf618b2cdd9
Giclayne <giclayne@gmail.com>
2022-12-15 15:43:26 -0300

    criacao do componente de exibir post

---
+++ src/components/PostDetail.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./PostDetail.module.css";
+const PostDetail = ({ post }) => {
+  return (
+    <div>
+      <img src={post.image} alt={post.title} />
+      <h2>{post.title}</h2>
+      <div>
+        {post && post.tags.map((tag) => (
+          <p key={tag}>
+            <span>#</span>
+            {tag}
+          </p>
+        ))}
+      </div>
+      <Link to={`/posts/${post.id}`} className="btn btn-outline">Ler</Link>
+    </div>
+  );
+};
+
+export default PostDetail;
