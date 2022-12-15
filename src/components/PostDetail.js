0dea33dd57ed2f668b749a731bb4b6fb2b5399b7
Giclayne <giclayne@gmail.com>
2022-12-15 16:25:32 -0300

    importando classes do css

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -3,10 +3,11 @@ import { Link } from "react-router-dom";
 import styles from "./PostDetail.module.css";
 const PostDetail = ({ post }) => {
   return (
-    <div>
+    <div className={styles.post_detail}>
       <img src={post.image} alt={post.title} />
       <h2>{post.title}</h2>
-      <div>
+      <p className={styles.createdby}>por: {post.createdBy}</p>
+      <div className={styles.tags}>
         {post && post.tags.map((tag) => (
           <p key={tag}>
             <span>#</span>
