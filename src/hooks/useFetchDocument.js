2d908fd05360570bfcabe14284239ef403a6518a
Giclayne <giclayne@gmail.com>
2022-12-19 21:42:29 -0300

    adicionando condicao do id do usuario

--- src/hooks/useFetchDocument.js
+++ src/hooks/useFetchDocument.js
@@ -33,7 +33,14 @@ export const useFetchDocuments = (docCollection, search = null, uid = null) => {
             where("tags", "array-contains", search),
             orderBy("createdAt", "desc")
           );
-        } else {
+        } else if(uid){
+          q = await query(
+            collectionRef,
+            where("uid", "==", uid),
+            orderBy("createdAt","desc")
+          )
+        }
+        else {
           q = await query(collectionRef, orderBy("createdAt", "desc"));
         }
         await onSnapshot(q, (querySnapshot) => {
