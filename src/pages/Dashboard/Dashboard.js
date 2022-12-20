3efa5a0a258f497821eeea83d4715ea930899a41
Giclayne <giclayne@gmail.com>
2022-12-19 22:03:03 -0300

    conclusao da estrutura da dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -5,14 +5,20 @@ import styles from "./Dashboard.module.css";
 
 //hooks
 import { useAuthValue } from "../../context/AuthContext";
-import { useFetchDocument, useFetchDocuments } from "../../hooks/useFetchDocument";
+import {
+  useFetchDocument,
+  useFetchDocuments,
+} from "../../hooks/useFetchDocument";
 
 const Dashboard = () => {
   const { user } = useAuthValue();
   const uid = user.uid;
 
   //posts do usuario
-  const {documents : posts, loading} = useFetchDocuments("posts", null, uid)
+  const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
+  const deleteDocument = (id) =>{
+
+  }
   return (
     <div>
       <h2>Dashboard</h2>
@@ -25,11 +31,21 @@ const Dashboard = () => {
           </Link>
         </div>
       ) : (
-        <div>
-          <p>Tem posts</p>
-        </div>
+        <>
+          <div>
+            <span>Título</span>
+            <span>Açoes</span>
+          </div>
+          {posts && posts.map((post) => <div key={post.id}>
+            <p>{post.title}</p>
+            <div>
+              <Link to={`/posts/${post.id}`} className="btn btn-outline">Ver</Link>
+              <Link to={`/posts/edit${post.id}`} className="btn btn-outline">Editar</Link>
+              <button onClick={()=> deleteDocument(post.id)} className="btn btn-outline btn-danger">Excluir</button>
+            </div>
+          </div>)}
+        </>
       )}
-      {posts && posts.map((post) => <h3>{post.title}</h3>)}
     </div>
   );
 };
