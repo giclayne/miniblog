1b011848d093413dcfef7f6bab96aad397b6d4af
Giclayne <giclayne@gmail.com>
2022-12-20 15:10:38 -0300

    importando rota de edicao de post

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import CreatePost from "./pages/CreatePost/CreatePost";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Search from "./pages/Search/Search";
 import Post from "./pages/Post/Post";
+import EditPost from "./pages/EditPost/EditPost";
 function App() {
   const [user, setUser] = useState(undefined);
   const { auth } = useAuthentication();
@@ -48,6 +49,10 @@ function App() {
               <Route
                 path="/register"
                 element={!user ? <Register /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/posts/edit/:id"
+                element={user ? <EditPost /> : <Navigate to="/login" />}
               />
               <Route
                 path="/posts/create"
