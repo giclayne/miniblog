6a537f10421fbccbcaa0f4d3fba7aec4560b99cc
Giclayne <giclayne@gmail.com>
2022-12-19 21:04:34 -0300

    mostrando todos os dados do post

--- src/pages/Post/Post.js
+++ src/pages/Post/Post.js
@@ -7,13 +7,23 @@ import { useFetchDocumentt } from "../../hooks/useFetchDocumentt";
 
 const Post = () => {
   const { id } = useParams();
-  const { document: post,loading } = useFetchDocumentt("posts", id);
+  const { document: post, loading } = useFetchDocumentt("posts", id);
   return (
     <div>
-        {loading && <p>Carregando post...</p>}
+      {loading && <p>Carregando post...</p>}
       {post && (
         <>
           <h1>{post.title}</h1>
+          <img src={post.image} alt={post.title} />
+          <p>{post.body}</p>
+          <h3>Este post trata sobre:</h3>
+          {post.tagsArray.map((tag) => (
+            <p key={tag}>
+              {" "}
+              <span>#</span>
+              {tag}{" "}
+            </p>
+          ))}
         </>
       )}
     </div>
